0f6e89e60f6fcd122c06044be15ffdddeb9b453f
Monalisa Barik <monalishabarik171@gmail.com>
2026-08-27 22:08:41 +0530

    report

--- src/App.jsx
+++ src/App.jsx
@@ -101,6 +101,7 @@ import NewJob from "./Pages/Jobs/NewJob";
 import CandidateDetails from "./Pages/Candidate/CandidateDetails";
 import RecruitmentWorkflow from "./Pages/RecruitmentWorkflow/RecruitmentWorkflow";
 import RecruitmentWorkflowDetails from "./Pages/RecruitmentWorkflow/RecruitmentWorkflowDetails";
+import JobRoleReport from "./Pages/Reports/JobRoleReport";
 
 import "./app.css";
 
@@ -192,6 +193,11 @@ function App() {
                         path="employees"
                         element={<Employees />}
                     />
+
+                    <Route
+    path="/dashboard/reports"
+    element={<JobRoleReport />}
+/>
 
                 </Route>
 
